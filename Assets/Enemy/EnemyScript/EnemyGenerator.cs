26c4c4138d81a8732e995ec90093e4781ee07792
kousei377 <kouseikun377@gmail.com>
2023-04-14 19:11:38 +0900

    エネミージェネレーター(仮)実装

--- Assets/Enemy/EnemyScript/EnemyGenerator.cs
+++ Assets/Enemy/EnemyScript/EnemyGenerator.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class EnemyGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public GameObject prehab;
+     IEnumerator Start()
+     {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            Instantiate(
+            //第一引数、参照するもの
+            prehab,
+            //第二引数、座標(x,y,z)
+                new Vector3(
+                    Random.Range(-45f, 45f),
+                    Random.Range(1f, 2f),
+                    Random.Range(-45f, 45f)
+                ),
+            Quaternion.identity
+            );
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
