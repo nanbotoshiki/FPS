79906415bbb2d710cba3773183d974cfb90fb222
kousei377 <kouseikun377@gmail.com>
2023-04-14 19:28:24 +0900

    エネミーコントローラー(仮)実装

---
+++ Assets/Enemy/EnemyScript/EnemyController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform player;
+    //索敵範囲(値=メートル)
+    public float traceDist = 20.0f;//とりあえず20m
+    NavMeshAgent nav;
+
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        StartCoroutine(CheckDist());
+    }
+
+    IEnumerator CheckDist()
+    {
+        while(true)
+        {
+            //1秒間に10回発見判定
+            yield return new WaitForSeconds(0.1f);
+            float dist =
+                Vector3.Distance
+                (player.position, transform.position);
+            //索敵範囲に入りましたか?
+            if(dist<traceDist)
+            {
+                //プレイヤーの位置を目的値に設定
+                nav.SetDestination(player.position);
+                nav.isStopped = false;
+            }
+            else
+            {
+                nav.isStopped = true;
+            }
+        }
+    }
+}
