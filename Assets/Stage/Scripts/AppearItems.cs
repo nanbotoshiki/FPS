8e8e0aa2f8a70ac9f2077aacc2ae3fe1e306a9fe
kina2760 <hattori.knr@gmail.com>
2023-04-19 20:10:37 +0900

    アイテム生成上限追加

--- Assets/Stage/Scripts/AppearItems.cs
+++ Assets/Stage/Scripts/AppearItems.cs
@@ -7,24 +7,32 @@ public class AppearItems : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public float span;
+    GameObject[] bulletArray;
+    public float itemLimit;
+    
     
  
     void Start()
     {
+        bulletArray = GameObject.FindGameObjectsWithTag("Bullet");
         StartCoroutine("AppearItem");
+        
     }
     IEnumerator AppearItem()
     {
-        while (true)
+        
+        while (bulletArray.Length < itemLimit)
         {
             yield return new WaitForSeconds(span);
+            bulletArray = GameObject.FindGameObjectsWithTag("Bullet");
             float x = Random.Range(-50.0f, 50.0f);
             float z = Random.Range(-47.0f, 45.0f);
             Vector3 randomPoint = new Vector3(x, 3.0f, z);
-            if (NavMesh.SamplePosition(randomPoint, out NavMeshHit navMeshHit, 10.0f, NavMesh.AllAreas))
+            if (NavMesh.SamplePosition(randomPoint, out NavMeshHit navMeshHit, 2.0f, NavMesh.AllAreas))
             {
                 Instantiate(bulletPrefab, navMeshHit.position, Quaternion.identity);
             }
+            Debug.Log(bulletArray.Length);
         }
     }
     // Update is called once per frame
