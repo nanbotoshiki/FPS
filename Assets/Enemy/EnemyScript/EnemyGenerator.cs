a9853fa3293e470912119d6a84ec3eefb86c85be
kousei377 <kouseikun377@gmail.com>
2023-04-19 18:53:00 +0900

    ゾンビが出てくる時プレイヤーの方を向くように設定(仮)

--- Assets/Enemy/EnemyScript/EnemyGenerator.cs
+++ Assets/Enemy/EnemyScript/EnemyGenerator.cs
@@ -10,6 +10,13 @@ public class EnemyGenerator : MonoBehaviour
     public int limit; //敵の最大数管理してます。現在10体
     int counter = 0;    //ゾンビの数を管理する予定です
 
+//ランダム配置でオブジェクトに重ならないように改造中。
+/*
+
+    
+
+ */
+
     IEnumerator Start()
     {
         while (true)
@@ -18,17 +25,19 @@ public class EnemyGenerator : MonoBehaviour
             yield return new WaitForSeconds(1.0f);
             if (counter < limit)
             {
-            GameObject enemy = Instantiate(
-            //第一引数、参照するもの
-            prehab,
-            //第二引数、座標(x,y,z)
+
+                GameObject enemy = Instantiate(
+                //第一引数、参照するもの
+                prehab,
+                //第二引数、座標(x,y,z)
                 new Vector3(
                     Random.Range(-45f, 45f),
                     Random.Range(1f, 2f),
                     Random.Range(-45f, 45f)
                 ),
-            Quaternion.identity
-            );
+                //第三引数、向き(デフォルト抜きの場合、Quaternion.identity)
+                Quaternion.LookRotation(player.position)
+                ) ;
             enemy.GetComponent<EnemyController>().Setplayer(player);
                 counter++;
             }
