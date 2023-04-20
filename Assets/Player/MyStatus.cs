2e8072f33a331062b060ce29fffb2858ac880533
Nanbo <nanbo.toshiki@gmail.com>
2023-04-20 19:13:01 +0900

    bullet取得可能

--- Assets/Player/MyStatus.cs
+++ Assets/Player/MyStatus.cs
@@ -1,29 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 
 
 public class MyStatus : MonoBehaviour
 {
-    private int shotCount = 50;
-    public int MaxShotCount = 100;
+    const int Defaulthp = 100;
+    int hp = Defaulthp;
+    Shooter ss;
 
-    public int SetShotCount(int num)
+    public Text bulletText;
+    public Text hpText;
+   
+    public int Hp
     {
-        if(num < MaxShotCount)
+        get
         {
-            return num;
+            return hp;
         }
-        else
+        set
         {
-            return MaxShotCount;
+            hp = value;
         }
-        
     }
-    public int GetShotCount()
+
+    bool IsDead()
     {
-        return shotCount;
+        return hp <= 0;
     }
+
+    private void Start()
+    {
+        ss = GameObject.Find("Shooter").GetComponent<Shooter>();
+    }
+
+    public void Update()
+    {
+        hpText.text = Hp.ToString();
+        bulletText.text = ss.ShotCount.ToString();
+
+    }
+
 }
--- Assets/Player/ShellItem.cs
+++ Assets/Player/ShellItem.cs
@@ -4,29 +4,32 @@ using UnityEngine;
 
 public class ShellItem : MonoBehaviour
 {
+    /*
     public AudioClip getSound;
-    public GameObject effectPrfab;
-    private Shooter ss;
+    public GameObject effectPrefab;
+    */
+    Shooter ss;
     private int reward = 20;
 
-    /*
-    private void OnTriggerEnter(Collider other)
+    
+    void OnCollisionEnter(Collision other)
     {
-        if(other.gamaObject.tag == "Player")
+        ss = GameObject.Find("Shooter").GetComponent<Shooter>();
+        if(other.gameObject.tag == "Player")
         {
             // Find()メソッドは、「名前」でオブジェクトを探し特定します。
             // ssオブジェクトを探し出し、それに付いているssスクリプト（component）のデータを取得。
             // 取得したデータを「ss」の箱の中に入れる。
-            ss = GameObject.Find("Shooter").GetComponent<Shooter>();
-            ss.AddShell(reward);
+            ss.ShotCount += reward;
             Destroy(gameObject);
-
-            AudioSource.PlayClipAtPoint(getSound, Camera.main.transform.position)
+            /*
+            AudioSource.PlayClipAtPoint(getSound, Camera.main.transform.position);
 
             GameObject effect = Instantiate(effectPrefab, transform.position, Quaternion.identity);
             Destroy(effect, 0.5f);
+            */
         }
     }
-    */
+    
 
 }
