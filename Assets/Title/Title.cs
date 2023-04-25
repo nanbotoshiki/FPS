a6d91589f22c03a674573f9152cff875a857c67c
kina2760 <hattori.knr@gmail.com>
2023-04-25 20:57:10 +0900

    難易度選択ボタン（お試し）

--- Assets/Title/Title.cs
+++ Assets/Title/Title.cs
@@ -6,6 +6,10 @@ using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
+    public Text easyText;
+    public Text normalText;
+    public Text hardText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +25,17 @@ public class Title : MonoBehaviour
     public void GameStart()
     {
         SceneManager.LoadScene("Stage1");
+    }
+
+    public void Right()
+    {
+        normalText.enabled = false;
+        hardText.enabled = true;
+    }
+
+    public void Left()
+    {
+        normalText.enabled = false;
+        easyText.enabled = true;
     }
 }
