3acc2a01711e4e2504401e827469a5e1d756b844
kina2760 <hattori.knr@gmail.com>
2023-04-28 19:11:17 +0900

    titleシーンにコマンドスクリプト追加

---
+++ Assets/Title/Konami.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Konami : MonoBehaviour
+{
+    int cmdSeq = 0;
+    int[] keyCodes;
+    int[] konamiCommand = new[] {
+        (int)KeyCode.UpArrow,
+        (int)KeyCode.UpArrow,
+        (int)KeyCode.DownArrow,
+        (int)KeyCode.DownArrow,
+        (int)KeyCode.LeftArrow,
+        (int)KeyCode.RightArrow,
+        (int)KeyCode.LeftArrow,
+        (int)KeyCode.RightArrow,
+        (int)KeyCode.B,
+        (int)KeyCode.A
+    };
+    int kcnt = 0;
+
+    private void Start()
+    {
+        keyCodes = (int[])Enum.GetValues(typeof(KeyCode));
+    }
+
+    void Update()
+    {
+        var len = keyCodes.Length;
+        for (var i = 0; i < len; i++)
+        {
+            if (Input.GetKeyUp((KeyCode)keyCodes[i]))
+            {
+                if (konamiCommand[cmdSeq] == keyCodes[i])
+                {
+                    cmdSeq++;
+                    if (cmdSeq == konamiCommand.Length)
+                    {
+                        kcnt++;
+                        print("konami!! " + kcnt);
+                        cmdSeq = 0;
+                    }
+                }
+                else
+                {
+                    cmdSeq = 0;
+                }
+            }
+        }
+    }
+}
